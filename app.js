c4c8c8baa3d56cdd1de53e1cbdd6c8096241dc77
Alice-MacAir <amchung@stanford.edu>
2013-10-16 12:02:38 -0700

    sub out of io

--- app.js
+++ app.js
@@ -51,19 +51,13 @@ var pub = redis.createClient();
 var sub = redis.createClient();
 var list = redis.createClient();
 
+sub.subscribe("chatting");
+sub.on("message", function (channel, message) {
+	console.log("message received on server from publish ");
+	client.send(message);
+});
+
 io.sockets.on('connection', function (client) {
-	/*sub_score.subscribe("postscore");
-	sub_score.on("score", function (channel, score) {
-		console.log("score received on server from publish ");
-		client.send(score);
-	});*/
-	
-	sub.subscribe("chatting");
-	sub.on("message", function (channel, message) {
-		console.log("message received on server from publish ");
-		client.send(message);
-	});
-	
 	list.zrevrange("myset", 0 , 4, 'withscores', function(err,members){
 		var lists=_.groupBy(members,function(a,b){
 			return Math.floor(b/2);
@@ -103,3 +97,45 @@ io.sockets.on('connection', function (client) {
 		pub.publish("chatting","Disconnected :" + client.id);
 	});
 });
+
+function setUser(username, img, area, expire){
+	client.set(area+':users:' + username, username);
+	client.expire(area+':users:' + username, expire);
+	client.set(area+':users:' + username + ':img', img);
+	client.expire(area+':users:' + username + ':img', expire);
+	//set a timer
+	client.set(area+':users:timer', expire);
+	client.expire(area+':users:timer', expire);
+	client.sadd(area+':users', area+':users:' + username);
+	//add the set to the expire set
+	client.sadd('expireKeys', area+':users');
+};
+
+
+function checkExpires(){
+	//grab the expire set
+	store.smembers('expireKeys', function(err, keys){
+		if(keys != null){
+			keys.forEach(function(key){
+				client.get(key+':timer', function(err, timer){
+					//grab the timer
+					if(timer != null){
+						//timer exists check the ttl on it
+						client.ttl(key+':timer', function(err, ttl){
+							//the ttl is two hours and if it is under
+							//a half hour we delete it
+							if(ttl < 1800){
+								client.del(key);
+								client.srem('expireKeys', key);
+							}
+						});
+					}else{
+						//the timer is gone delete the key
+						client.del(key);
+						client.srem('expireKeys', key);
+					}
+				})
+			});
+		}
+	});
+};
