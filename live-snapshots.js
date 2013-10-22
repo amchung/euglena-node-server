8dc32358e0d479cafe939aa83cce18130ccea2d6
Alice-MacAir <amchung@stanford.edu>
2013-10-22 11:26:50 -0700

    start writing live snapshot

--- live-snapshots.js
+++ live-snapshots.js
@@ -16,15 +16,30 @@ var vid_width = 640
 
 function getMjpeg(){
     var img = new Image();
-        img.onload = function() {
-            ctx.clearRect(0, 0, vid_width, vid_height);
-            ctx.drawImage(img, 0, 0, vid_width, vid_height);
-            // motion detection
-            //compareFrame(img);
-            // load frame
-            //requestAnimFrame(getMjpeg);
-        };
-        img.src = "http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime();
+        
+    img.onerror = function(err){
+  		throw err;
+	};
+
+	img.onload = function(){
+  		var w = img.width / 2
+    	, h = img.height / 2
+    	, canvas = new Canvas(w, h)
+    	, ctx = canvas.getContext('2d');
+
+  		ctx.drawImage(img, 0, 0, w, h, 0, 0, w, h);
+
+ 	 	//var out = fs.createWriteStream(__dirname + '/crop.jpg');
+
+  		//var stream = canvas.createJPEGStream({
+  		//  	bufsize : 2048,
+ 		//   	quality : 80
+ 	 	//});
+
+	  	//stream.pipe(out);
+	};
+
+	img.src = "http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime();
 }
 
 http.createServer(function (req, res) {
