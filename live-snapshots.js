971d13eea1da59c32598fa16491d845cd7a8766e
Alice-MacAir <amchung@stanford.edu>
2013-10-22 13:50:00 -0700

    fixing live-snapshot

--- live-snapshots.js
+++ live-snapshots.js
@@ -2,13 +2,16 @@
 /**
  * Module dependencies.
  */
-
-var Canvas = require('canvas')
-  , canvas = new Canvas(320, 320)
-  , ctx = canvas.getContext('2d')
-  , Image = Canvas.Image;
   
 var http = require('http');
+
+var vid_width = 640
+  , vid_height = 480;
+  
+var Canvas = require('canvas')
+  , canvas = new Canvas(vid_width, vid_height)
+  , ctx = canvas.getContext('2d')
+  , Image = Canvas.Image;
   
 function takeSnapshot(){
 	var timestamp = new Date().getTime();
@@ -35,69 +38,6 @@ function takeSnapshot(){
     	console.log("Got error: " + e.message);
     });
 }
-
-/*  
-var http = require('http')
-  , fs = require('fs')
-  , url = require('url');
-
-var options = {
-    host: '171.65.102.132'
-  , port: 8080
-  , path: '/?action=snapshot?t=' + new Date().getTime()
-}
-
-function getMjpeg(){
-	http.get(options, function(res){
-    	var imagedata = '';
-    	res.setEncoding('binary');
-		var data = new Buffer(parseInt(res.headers['content-length'],10));
-	
-    	res.on('data', function(chunk){
-    		chunk.copy(data, imagedata);
-        	imagedata += chunk;
-    	});
-
-    	res.on('end', function () {
-  			//img = new canvas.Image;
-  			var img = new Image();
-  			
-  			img.onerror = function(err){
-  				throw err;
-			};
-
-  			img.onload = function(){
-				ctx.drawImage(img, 0, 0, img.width, img.height);
-			};
-			
-			img.src = imagedata;
-		});
-	});
-}*/
-
-
-/*var vid_width = 640
-  , vid_height = 480;
-
-
-function getMjpeg(){
-    var img = new Image();
-        
-    img.onerror = function(err){
-  		throw err;
-	};
-
-	img.onload = function(){
-  		var w = img.width / 2
-    	, h = img.height / 2
-    	, canvas = new Canvas(w, h)
-    	, ctx = canvas.getContext('2d');
-
-  		ctx.drawImage(img, 0, 0, w, h, 0, 0, w, h);
-	};
-
-	img.src = "http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime();
-}*/
 
 http.createServer(function (req, res) {
   takeSnapshot();
